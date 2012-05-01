0287de3404688630911d35c6e47d2044455c377e
Jesse Bounds <jesse@rebounds.net>
2012-04-30 17:16:42 -0700

    adding to github

--- munirt.js
+++ munirt.js
@@ -4,10 +4,11 @@ Alerts = new Meteor.Collection("Alerts");
 if (Meteor.is_client) {
 
   yepnope({
-    load: ["http://leaflet.cloudmade.com/dist/leaflet.js", "http://leaflet.cloudmade.com/dist/leaflet.css"],
+    load: ["http://leaflet.cloudmade.com/dist/leaflet.js", 
+           "http://leaflet.cloudmade.com/dist/leaflet.css"],
     complete: function () {
       var map = new L.Map('map');
-      var mapCenter = new L.LatLng(37.730599,-122.44339);
+      var mapCenter = new L.LatLng(37.730599, -122.44339); 
       var mapZoom = 12;
       var baseUrl = 'http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.jpg',
           attrib = 'Map tiles by MapQuest',
@@ -16,32 +17,25 @@ if (Meteor.is_client) {
       map.setView(mapCenter, mapZoom);
       var group = new L.LayerGroup();
       map.addLayer(group);
-
+      function updateMap(map, group, munis, L) {
+        if (map === null)
+          return;
+        group.clearLayers();
+        munis.forEach(function(muni) {
+          var latlng = new L.LatLng(muni.lat, muni.lon);
+          var marker = new L.CircleMarker(latlng);
+          marker.bindPopup("<h3>" + muni.id + "</h3>");
+          marker.setRadius(4);
+          group.addLayer(marker);
+        });
+      }
       Alerts.find({name: "alert"}).observe({
             added: function () {
               updateMap(map, group, Munis.find(), L);
-            },
-            removed: function () {
-              console.log("removed");
-            },
-            changed: function () {
-              console.log("changed");
             }
       });
     }
   });
-
-  function updateMap(map, group, munis, L) {
-    if (map === null)
-      return;
-    group.clearLayers();
-    munis.forEach(function(muni) {
-      var latlng = new L.LatLng(muni.lat, muni.lon);
-      var marker = new L.CircleMarker(latlng);
-      marker.setRadius(4);
-      group.addLayer(marker);
-    });
-  }
 
   Template.locations.munis = function () {
     var munis = Munis.find();
@@ -54,29 +48,29 @@ if (Meteor.is_server) {
   Meteor.startup(function () {
     console.log("munis server starting...");
     console.log("starting poll of muni real-time service...");
-
     update();
-
     function update() {
-      Meteor.http.call("GET", "http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=sf-muni&r=N&t=0", {}, function(error, result) {
-
-        munis = xml2json.parser(result['content']);
-
-        Alerts.remove({});
-        for (var i=0; i<munis['body']['vehicle'].length; i++) {
-          muni = munis['body']['vehicle'][i];
-          if (Munis.find({id: muni['id']}).count() === 0) {
-            console.log("muni id: " + muni['id'] + " not found; inserting");
-            Munis.insert(muni);
-          } else { 
-            //console.log("muni id: " + muni['id'] + " found; updating");
-            Munis.update({id: muni['id']}, {$set: {lat: muni['lat'], lon: muni['lon'], secsSinceReport: muni['secssincereport']}});
+      Meteor.http.call("GET", 
+        "http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=sf-muni&r=N&t=0", 
+        {}, 
+        function(error, result) {
+          munis = xml2json.parser(result['content']);
+          Alerts.remove({});
+          for (var i=0; i<munis['body']['vehicle'].length; i++) {
+            muni = munis['body']['vehicle'][i];
+            if (Munis.find({id: muni['id']}).count() === 0) {
+              console.log("muni id: " + muni['id'] + " not found; inserting");
+              Munis.insert(muni);
+            } else { 
+              Munis.update({id: muni['id']}, 
+                           {$set: {lat: muni['lat'], 
+                                   lon: muni['lon'], 
+                       secsSinceReport: muni['secssincereport']}});
+            }
           }
-        }
         Alerts.insert({name: "alert"});
         Meteor.setTimeout(update, 5000);
       });
-
     }
 
   });
